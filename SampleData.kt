4bda74730f4a7ab6f315b2a29cf10e0a94aa17a7
elenaberegova <elenaberegova@proton.me>
2026-07-22 17:35:08 +0300

    Create SampleData.kt

---
+++ SampleData.kt
@@ -0,0 +1,64 @@
+object SampleData {
+
+    fun load() = listOf(
+
+        Technology(
+            "MS-DOS",
+            "Operating System",
+            1981,
+            "Command-line operating system."
+        ),
+
+        Technology(
+            "AmigaOS",
+            "Operating System",
+            1987,
+            "Classic multimedia desktop OS."
+        ),
+
+        Technology(
+            "CP/M",
+            "Operating System",
+            1974,
+            "Popular microcomputer operating system."
+        ),
+
+        Technology(
+            "BASIC",
+            "Programming Language",
+            1964,
+            "Educational programming language."
+        ),
+
+        Technology(
+            "Pascal",
+            "Programming Language",
+            1970,
+            "Structured programming language."
+        ),
+
+        Technology(
+            "ALGOL",
+            "Programming Language",
+            1958,
+            "Algorithmic language."
+
+        ),
+
+        Technology(
+            "WAV",
+            "File Format",
+            1991,
+            "Digital audio format."
+        ),
+
+        Technology(
+            "XBM",
+            "File Format",
+            1985,
+            "Bitmap image format."
+        )
+
+    )
+
+}
